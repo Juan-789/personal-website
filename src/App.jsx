6ff1c21a07698885a19c3de117309c937f108b20
Juan-789 <juan.alfredoprincipal@gmail.com>
2025-10-30 18:27:24 -0400

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import zaratusthra from '/zarathustra.jpg'
 import outsider from '/the outsider.jpg'
 import white_nights from '/white_nights.jpg'
 import './App.css'
 
-
-function openTab(evt, tabName) {
-  var i, tabContent, tablinks;
-  tabContent = document.getElementsByClassName("tabcontent");
-  for (i=0; i< tabContent.length; i++) {
-    tabContent[i].computedStyleMap.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i< tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-
-}
-
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen)
+  }
 
   return (
-    <>
+    <div>
     {/* this div should be like the top Hello */}
-      <div> 
+      <div>
+        {/* <button onClick={toggleSidebar}>
+          {isSidebarOpen ? 'Close Sections' : 'Open Sections'}
+        </button> */}
+
+        <Sidebar isOpen={isSidebarOpen}>
+          <h3>Page Navigation</h3>
+          <ul>
+            <li>Home</li>
+            <li>Reading</li>
+            <ul>
+              <li>Essays</li>
+              <li>Books</li>
+              <li>To be read</li>
+              <li>Notes</li>
+            </ul>
+            <li>Building</li>
+            <ul>
+              <li>Projects</li>
+              <li>Learning</li>
+            </ul>
+            <li>Exploring</li>
+            <ul>
+            <li>Community</li>
+            <li>Food</li>
+            <li>Movies</li>
+            <li>Cool links</li>
+            </ul>
+          </ul>
+        </Sidebar>
+      </div>
+      <div style={{marginLeft: isSidebarOpen ? '250px' : '0',
+          transition: 'margin-left 0.3s ease-in-out' }}>
+
+      <div > 
         <About />
       </div>
-      {/* <div class="tab">
-        <button class="tablinks" onClick={openTab(event, "About")}>About</button>
-        <button class="tablinks" onClick={openTab(event, "Projects")}>Projects</button>
-        <button class="tablinks" onClick={openTab(event, "Experience")}>Experience</button>
-        <button class="tablinks" onClick={openTab(event, "Blog")}>Blog</button>
+        <Blog />
+        <Project />
       </div>
-
-
-      <div id="About" class="tabContent">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <div id="Projects" class="tabContent">
-
-      </div>
-      <div id="Experience" class="tabContent">
-
-      </div>
-      <div id="Blog" class="tabContent">
- */}
-      {/* </div> */}
-      <Blog />
-      <Project />
-    </>
+    </div>
   )
 }
 
@@ -120,9 +119,9 @@ function Blog() {
 function About() {
   return (
     <div>
-      <h1>Hello I'm Juan</h1> 
+      <h1>Hi I'm Juan :)</h1> 
       <p>
-        I like to learn how stuff works, and build indie, fun programs
+        I like to learn how stuff works, and build indie, fun programs <i>Just for the sake of it</i>
       </p>
     </div>
   )
@@ -134,6 +133,34 @@ function Project() {
       <h1>These are some of my projects</h1>
     </div>
   )
+}
+
+function Food(){} //places i've gone, and or maybe recipes,
+
+function ToBeRead(){} //books in my shelf i need to read
+
+function Movies(){} //movies i've watched that I like
+
+function CoolLinks(){}  // cool websites, inspiration, fun facts, etc...
+
+function interestingThingsImLearningAbout(){} // how a cd works, how does it read, and write, the stars, etc...
+
+function Notes(){}  // notes i've taken when learning something, handwritten or digital, notion or something
+
+function Experience() {}
+// 1. Define the component and accept props for content and state
+function Sidebar({ isOpen, children }) {
+  // 2. Add classes dynamically based on the 'isOpen' prop
+  const sidebarClasses = `sidebar ${isOpen ? 'open' : 'closed'}`;
+
+  return (
+    <div className={sidebarClasses}>
+      {/* 3. Render any passed content */}
+      <div className="sidebar-content">
+        {children}
+      </div>
+    </div>
+  );
 }
 
 export default App
