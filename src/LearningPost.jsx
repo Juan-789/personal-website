886ce3e5d766575326cfbc10b0a20cfcf351a9a8
Juan-789 <juan.alfredoprincipal@gmail.com>
2025-11-04 23:50:35 -0500

    how compression works, initial video

---
+++ src/LearningPost.jsx
@@ -0,0 +1,30 @@
+
+// the importance of name
+export function HowFileCompressionWorks(){
+    return (
+        <div>
+            <h2>How Does File Compression work?</h2>
+        <div>
+            <h3>Curiosity</h3>
+        </div>
+        
+        <div>
+            When one learns something, usually one learns with a toy example or one that avoids all the
+            complications and edge cases, and sometimes with an example of our common lives, and the easiest
+            explanation for how compression works was obviously found in a video from 11 years ago,
+            <div>
+            <iframe 
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/R0d-rKYWKWw?si=iGzeHmJvphVHJesp" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+            </iframe>
+            </div>
+        </div>            
+        </div>
+    );
+} 
