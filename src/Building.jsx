6464949b8947c1b844d9edcb17ab472f49ae705e
juan <juanmarulandadelosri@cmail.carleton.ca>
2026-09-24 14:49:45 -0400

    had to adapt for windows

--- src/Building.jsx
+++ src/Building.jsx
@@ -3,7 +3,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
-import job from '/j*b.jpg'
+import job from '/job.jpg'
 import wok from '/wok.gif'
 import proj from '/projects.JPG'
 import { Margin } from '@mui/icons-material';
@@ -78,14 +78,14 @@ export default function Building() {
             component="img"
             height="300"
             image={job}
-            alt="J*b"
+            alt="Job"
           />
           <CardContent className='glass-card-content'>
             <Typography gutterBottom variant="h5" component="div">
               Employment
             </Typography>
             <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              J*b 
+              J*b
             </Typography>
           </CardContent>
         </CardActionArea>
